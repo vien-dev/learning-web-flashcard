0a05000a11c4dcf4ec4c573ab92aeef3bf8f8109
Vien Mai <56462929+vien-dev@users.noreply.github.com>
2023-03-24 01:09:00 +0100

    Finish implementing Carousel of top picked words

--- app.js
+++ app.js
@@ -8,8 +8,27 @@ app.set("view engine", 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+let topPickFlashCards = [
+    { word:'hej',
+      defintion: 'A way to say hi',
+      example: 'Hej! hur mår du?'},
+    { word: 'hej då',
+      defintion: 'Goodbye!',
+      example: 'hej då! vi ses i morgon!'},
+    { word: 'titta',
+      defintion: 'Look!',
+      example: 'Titta! Bakom dig! (look! behind you!'},
+    { word: 'sitta',
+      defintion: 'Sit!',
+      example: 'Sitta här! (sit down here!'},
+    { word:'äpple',
+      defintion: 'Apple',
+      example: 'Titta! ett äpple!'}];
+
 app.get("/", function(req, res) {
-    res.render("home");
+    res.render("home", {
+        homeEJSTopPickFlashCards: topPickFlashCards.map(flashCard => flashCard.word)
+    });
 })
 
 app.get("/about", function(req, res) {
